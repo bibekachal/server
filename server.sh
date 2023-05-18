9250045cc4f13d57ba2f548cd5150624d342b319
bibekachal <133992841+bibekachal@users.noreply.github.com>
2023-05-18 23:36:33 +0545

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 #Script to install essentials
-
-# you MUST MUST MUST upgrade server with yum upgrade and then yum update otherwise it does not work no chmod command!
+#You MUST MUST MUST upgrade server with yum upgrade and then yum update otherwise it does not work no chmod command!
 yum upgrade
 yum update
 read -p 'Do you want to disable SE linux (later restart server manually) y/n: ' selinux
@@ -31,50 +29,37 @@ sed -i 's/Require method.*/Require all granted/' $CONF
 
 
 systemctl start httpd.service
-
 firewall-cmd --permanent --add-service=http
 firewall-cmd --permanent --add-service=https
 firewall-cmd --reload
-
 systemctl enable httpd.service
 
 
 #MariaDB
+echo 'Installing MariaDB...'
 yum -y install mariadb-server mariadb
-
-#EPEL AND REMI
-yum -y install epel-release
-yum -y install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
-
 systemctl start mariadb
 systemctl enable mariadb.service
-
 mysql_secure_installation
+echo 'Mariadb installed and started.';
 
 
 #PHP
+echo 'Installing PHP...'
 yum -y install php
 yum -y install php-gd php-pear php-mbstring
 yum -y install php-gd php-ldap php-odbc php-pear php-xml php-xmlrpc php-mbstring php-snmp php-soap curl curl-devel
+echo 'PHP installed.'
 
 #phpMyAdmin
-#phpMyAdmin is not default with centos8
-#thsese links did not work centos stream 9, worked once and did not work later so don't use these commented lines
-#dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-#dnf -y install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
-#dnf -y install http://rpms.remirepo.net/enterprise/8/remi/x86_64/php-fedora-autoloader-1.0.0-5.el8.remi.noarch.rpm
 #Guide link for extra info https://wiki.crowncloud.net/?How_to_Install_phpMyAdmin_on_CentOS_Stream_9
+echo 'Installing phpMyAdmin...'
 dnf config-manager --set-enabled crb
-
 dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-
 dnf install https://dl.fedoraproject.org/pub/epel/epel-next-release-latest-9.noarch.rpm
+dnf install phpMyAdmin
 
-# dnf config-manager --set-enabled remi
-#dnf install phpMyAdmin
-yum -y --enablerepo=remi install phpMyAdmin
-
-##Fix phpMyAdmin Conf
+#Fix phpMyAdmin Conf
 sed -i '/^<Directory.*phpMyAdmin\/>/,/^<\/Directory>/c\
 <Directory /usr/share/phpMyAdmin/>\
 AddDefaultCharset UTF-8\
@@ -107,12 +92,15 @@ sed -i '/^<Directory.*setup\/>/,/^<\/Directory>/c\
 </IfModule>\
 </Directory>' /etc/httpd/conf.d/phpMyAdmin.conf
 
-
+echo 'phpMyAdmin is installed.'
 
 #ftp
+echo 'Installing VSFTPD...';
 yum -y install vsftpd ftp
 systemctl enable vsftpd
 systemctl start vsftpd
+
+echo 'VSFTPD is installed and enabled.'
 
 firewall-cmd --permanent --add-port=21/tcp
 firewall-cmd --permanent --add-port=20/tcp
