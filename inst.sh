7a4daee6c687d4f5f0c152ba21ca26c95a9f77f9
bibekachal <133992841+bibekachal@users.noreply.github.com>
2023-05-18 22:47:11 +0545

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #Initial code
-#curl -O https://raw.githubusercontent.com/bibekachal/server/main/inst.sh?token=GHSAT0AAAAAACC2FP4GLGWFDGULHPRE5HPWZDGLB7A
+#curl -O https://raw.githubusercontent.com/bibekachal/server/main/inst.sh
 #dos2unix inst.sh
 #chmod +x inst.sh
 #sh inst.sh
 
-curl -O https://raw.githubusercontent.com/bibekachal/server/main/server.sh?token=GHSAT0AAAAAACC2FP4GNEKDJEYZY6IGZFV4ZDGK6WA
-curl -O https://raw.githubusercontent.com/bibekachal/server/main/user.sh?token=GHSAT0AAAAAACC2FP4GW3GO5NAQBQP3H65EZDGK7VQ
-curl -O https://raw.githubusercontent.com/bibekachal/server/main/vhost.sh?token=GHSAT0AAAAAACC2FP4H7TAXF6F7XW5CVNXCZDGLADA
+curl -O https://raw.githubusercontent.com/bibekachal/server/main/server.sh
+curl -O https://raw.githubusercontent.com/bibekachal/server/main/user.sh
+curl -O https://raw.githubusercontent.com/bibekachal/server/main/vhost.sh
 
 yum install dos2unix
 
