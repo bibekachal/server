bf7cf335a5d44e00ac43da3d3961517ab40820d8
bibekachal <133992841+bibekachal@users.noreply.github.com>
2023-09-06 20:28:33 +0545

    Update user.sh

--- user.sh
+++ user.sh
@@ -3,7 +3,7 @@
 #Add the user into the system creating the home directory as well (-m); this also creates the users's group with the name of the username
 
 #Script to add user
-read -p 'Do you want create a new user and assign apache to the user\'s group(y/n): ' con
+read -p 'Do you want create a new user(y/n): ' con
 
 if [ $con = 'y' ] ; then
 read -p 'Write username: ' username
