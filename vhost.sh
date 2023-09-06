3297900cce67d63399898e77810351cb81db2032
bibekachal <133992841+bibekachal@users.noreply.github.com>
2023-09-06 15:34:12 +0545

    Update vhost.sh

--- vhost.sh
+++ vhost.sh
@@ -5,27 +5,27 @@ read -p 'Server IP:' ip
 
 WEBDIR=/home/$username/web/$domain/public_html/
 CONFDIR=/etc/httpd/conf.d
-sudo [ -d $CONFDIR ] || mkdir -p $CONFDIR
+[ -d $CONFDIR ] || mkdir -p $CONFDIR
 
-sudo echo "<VirtualHost $ip:80>
+echo "<VirtualHost $ip:80>
       ServerName www.$domain
       ServerAlias $domain
       DocumentRoot "$WEBDIR"
     </VirtualHost>" > $CONFDIR/$domain.conf
 
-sudo mkdir -p $WEBDIR
+mkdir -p $WEBDIR
 
-sudo chmod 711 -R /home/$username
-sudo chmod 755 -R /home/$username/web/$domain/public_html
+chmod 711 -R /home/$username
+chmod 755 -R /home/$username/web/$domain/public_html
 
-sudo chown $username -R /home/$username
+chown $username -R /home/$username
 
 echo "New site for $domain" > $WEBDIR/index.html
 
 echo 'Restarting apache'
-sudo apachectl restart
-sudo systemctl restart httpd.service
-echo "Reboot server manually with reboot; you must do that if it's the first installation with SE Linux disabled"
+apachectl restart
+systemctl restart httpd.service
+"Reboot server manually with reboot; you must do that if it's the first installation with SE Linux disabled"
 exit 0
 
 #In wordpress script add define('FS_METHOD','direct'); above DB_NAME to remove ftp error; another error occurred after doing this
